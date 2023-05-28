c661119a3b9ff32e2b77da593fda5d561a2d36b8
Guilherme Salkovski <guilherme.salkovski@gmail.com>
2023-05-28 17:29:01 -0300

    2 - Component: Sidebar (finishing)

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import styles from './Sidebar.module.css'
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
     return (
@@ -10,13 +11,18 @@ export function Sidebar() {
             />
             
             <div className={styles.profile}>
+                <img className={styles.avatar} 
+                    src="https://github.com/GuilhermeSalkovski.png" 
+                    alt="" 
+                />
                 <strong>Guilherme Salkovski</strong>
                 <span>Software Developer</span>
             </div>
 
             <footer>
                 <a href="#">
-                    Editar seu perfil
+                   <PencilLine size={20} /> 
+                   Editar seu perfil
                 </a>
             </footer>
 
