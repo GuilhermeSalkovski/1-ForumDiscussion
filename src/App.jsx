1fa61392da99d13015365135c8c27d8373df401f
Guilherme Salkovski <guilherme.salkovski@gmail.com>
2023-05-28 16:27:38 -0300

    2 - Compoonent: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,29 @@
-import { Post } from "./Post"
-import { Header } from "./components/header"
-import './global.css'
+import { Post } from "./Post";
+import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
+
+import styles from './components/App.module.css';
+import './global.css';
 
 export function App() {
   return (
-  //  <h1>Hello Ignite!</h1>
-  <div>
-    < Header />
-    <Post 
-      author="Guilherme Salkovski" 
-      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem esse enim earum soluta similique commodi maiores inventore autem? Ducimus voluptatibus placeat aliquam eum quaerat at voluptatum, fuga ut ipsa accusamus."
-    />
-    
-    <Post
-      author="Thiago Felipe"
-      content="Outro post bem divertido"
-    />
-  </div>
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+            <Post 
+              author="Guilherme Salkovski" 
+              content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem esse enim earum soluta similique commodi maiores inventore autem? Ducimus voluptatibus placeat aliquam eum quaerat at voluptatum, fuga ut ipsa accusamus."
+            />
+          
+            <Post
+              author="Thiago Felipe"
+              content="Outro post bem divertido"
+            />
+        </main>
+      </div>
+    </div>
   )
 }
 
