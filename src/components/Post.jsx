36fd490fbf335f6c6b22771451df6d89708c04d2
Guilherme Salkovski <guilherme.salkovski@gmail.com>
2023-05-28 18:21:36 -0300

    2 - Component: Post

---
+++ src/components/Post.jsx
@@ -0,0 +1,33 @@
+import styles from './Post.module.css'
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} 
+                        src="https://github.com/GuilhermeSalkovski.png" 
+                        alt="" 
+                    />
+
+                    <div className={styles.authorInfo}>
+                        <strong>Guilherme Salkovski</strong>
+                        <span>Software Developer</span>
+                    </div>
+                </div>
+                <time title="28 de maio de 2023 às 09:51h" dateTime="2023-05-28 09:51:32">Publicado há 1h</time>
+            </header>
+
+            <div className={styles.content}>
+                <p>Fala galeraa 👋</p> 
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                <p><a href="#">jane.design/doctorcare</a></p>
+                <p>
+                    <a href="#">#novoprojeto</a> {' '}
+                    <a href="#">#nlw</a> {' '}
+                    <a href="#">#rocketseat</a> {' '}
+                </p>  
+            </div>
+        </article>
+    )
+}
