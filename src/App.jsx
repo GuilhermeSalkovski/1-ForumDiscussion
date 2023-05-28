6d34894962414e93391cc3c66d1932b66d7f98bf
Guilherme Salkovski <guilherme.salkovski@gmail.com>
2023-05-28 10:02:45 -0300

    2 - App Structure (initial commit)

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Post } from "./Post"
+import { Header } from "./components/header"
+import './styles.css'
+
+export function App() {
+  return (
+  //  <h1>Hello Ignite!</h1>
+  <div>
+    < Header />
+    <Post 
+      author="Guilherme Salkovski" 
+      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem esse enim earum soluta similique commodi maiores inventore autem? Ducimus voluptatibus placeat aliquam eum quaerat at voluptatum, fuga ut ipsa accusamus."
+    />
+    
+    <Post
+      author="Thiago Felipe"
+      content="Outro post bem divertido"
+    />
+  </div>
+  )
+}
+
