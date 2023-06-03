abefcf1fca0d77769caa095fd04b1b4b625d0047
Guilherme Salkovski <guilherme.salkovski@gmail.com>
2023-06-03 12:26:35 -0300

    3 - Iteration, props, dateTime formatting

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,49 @@ import { Sidebar } from "./components/Sidebar";
 import styles from './components/App.module.css';
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/GuilhermeSalkovski.png',
+      name: 'Guilherme Salkovski',
+      role: 'Software Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: `Acabei de subir mais um projeto no meu portifa. 
+                                      É um projeto que fiz no NLW Return,
+                                      evento da Rocketseat. O nome do projeto é DoctorCare 🚀`}, 
+      { type: 'link', content: 'guilherme.aloalo/doctorcare'},
+      // '#novoprojeto ',
+      // '#nlw',
+      // '#rocketseat '
+    ],
+    publishedAt: new Date('2023-05-30 20:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @ Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: `Acabei de subir mais um projeto no meu portifa. 
+                                      É um projeto que fiz no NLW Return,
+                                      evento da Rocketseat. O nome do projeto é DoctorCare 🚀`}, 
+      { type: 'link', content: 'guilherme.aloalo/doctorcare'},
+      // '#novoprojeto ',
+      // '#nlw',
+      // '#rocketseat '
+    ],
+    publishedAt: new Date('2023-06-03 10:00:00')
+  }
+];
+
+
+// Iteraçao!!!
 export function App() {
   return (
     <div>
@@ -12,15 +55,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-            <Post 
-              author="Guilherme Salkovski" 
-              content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem esse enim earum soluta similique commodi maiores inventore autem? Ducimus voluptatibus placeat aliquam eum quaerat at voluptatum, fuga ut ipsa accusamus."
-            />
-            
-            <Post
-              author="Thiago Felipe"
-              content="Outro post bem divertido"
-            />
+          {posts.map(post => {
+            return (
+              < Post 
+                author={post.author} 
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,31 +1,55 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
+
 import styles from './Post.module.css'
 
-export function Post() {
+export function Post({ author, publishedAt, content }) {
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    })
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/GuilhermeSalkovski.png" />
+                    <Avatar src={author.avatarUrl} />
 
                     <div className={styles.authorInfo}>
-                        <strong>Guilherme Salkovski</strong>
-                        <span>Software Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
-                <time title="28 de maio de 2023 às 09:51h" dateTime="2023-05-28 09:51:32">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p> 
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type === 'link'){
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
+
+                {/* <p>Fala galeraa 👋</p> 
+                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, 
+                    evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
                 <p><a href="#">guilherme.aloalo/doctorcare</a></p>
                 <p>
                     <a href="#">#novoprojeto</a> {' '}
                     <a href="#">#nlw</a> {' '}
                     <a href="#">#rocketseat</a> {' '}
-                </p>  
+                </p>   */}
             </div>
             <form className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
