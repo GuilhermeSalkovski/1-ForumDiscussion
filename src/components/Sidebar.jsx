5ecafccca8225abcca562eab5e09f49ed67e7eb0
Guilherme Salkovski <guilherme.salkovski@gmail.com>
2023-06-03 01:03:31 -0300

    2 - Component: Avatar + Responsivity

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import styles from './Sidebar.module.css'
 import { PencilLine } from 'phosphor-react'
 
@@ -11,10 +12,7 @@ export function Sidebar() {
             />
             
             <div className={styles.profile}>
-                <img className={styles.avatar} 
-                    src="https://github.com/GuilhermeSalkovski.png" 
-                    alt="" 
-                />
+                <Avatar src="https://github.com/GuilhermeSalkovski.png" />
                 <strong>Guilherme Salkovski</strong>
                 <span>Software Developer</span>
             </div>
